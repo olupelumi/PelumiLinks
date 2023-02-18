6f45cd7e365035d6439fb06667cf6555c0c3b590
Oluwapelumi Fafowora <oluwapelumifafowora@Oluwapelumis-MacBook-Pro-2.local>
2023-02-17 20:12:12 -0800

    implement show and control actions

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,11 +4,16 @@ class UrlsController < ApplicationController
         render json: @urls
     end
 
-    def show 
-    end 
+    def show
+        @url = Url.find_by(shortcut: params[:shortcut])
+        render json: @url
+    end
 
     def create 
-
+        @url = Url.create(shortcut: params[:shortcut], long_url: params[:long_url])
+        # TODO: make it so we don't have duplicate articles. This may be done in the model as a validation instead though. 
+        # TODO: Add some error handling
+        render json: Url.all
     end 
 
     def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "urls#index"
-  resources :urls 
+  resources :urls, param: :shortcut
 end
