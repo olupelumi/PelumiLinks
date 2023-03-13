6443eb03a6100d9f201c74a86c82359333780005
Oluwapelumi Fafowora <oluwapelumifafowora@Oluwapelumis-MacBook-Pro-2.local>
2023-03-12 18:26:49 -0700

    more todo for frontend

--- frontend/pelu_links_frontend/src/App.js
+++ frontend/pelu_links_frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   // TODO: fetch shorcut information, create ui to show shortcuts and longurls,
   // Have the proper shortcuts and urls actually show up in the ui
+  // Then think about how to make the fetched information persist
   return (
     <>
     <h1>Pelumi Links</h1>
