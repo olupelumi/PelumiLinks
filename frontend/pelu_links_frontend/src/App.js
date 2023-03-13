d4c88c79ac19b72dcf5deb9215b02f4e99a49341
Oluwapelumi Fafowora <oluwapelumifafowora@Oluwapelumis-MacBook-Pro-2.local>
2023-03-12 22:09:15 -0700

    api request attempt

--- frontend/pelu_links_frontend/src/App.js
+++ frontend/pelu_links_frontend/src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
   // TODO: fetch shorcut information, create ui to show shortcuts and longurls,
   // Have the proper shortcuts and urls actually show up in the ui
   // Then think about how to make the fetched information persist
+
+  const [urls, setUrls] = useState();
+
+  // TODO fix cors error from the below code
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:3000')
+      .then(response => {
+        setUrls(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+
   return (
     <>
     <h1>Pelumi Links</h1>
