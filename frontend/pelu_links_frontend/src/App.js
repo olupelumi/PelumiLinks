899304aa5084b2fbfa5a7d3628cb3153d7ec38a1
Oluwapelumi Fafowora <oluwapelumifafowora@Oluwapelumis-MacBook-Pro-2.local>
2023-04-06 17:51:07 -0700

    update edit modal

--- frontend/pelu_links_frontend/src/App.js
+++ frontend/pelu_links_frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios'
-import {KeyboardArrowDown,  KeyboardArrowUp, Edit, Delete} from '@mui/icons-material';
+import {KeyboardArrowDown,  KeyboardArrowUp, Edit, Delete, Close} from '@mui/icons-material';
 
 const UrlBlockMobile = ({shortcut, long_url, setShowEdit, setShowDelete}) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -86,14 +86,21 @@ const UrlEdit = ({shortcut, long_url, setShowEdit, setUrls}) => {
     <>
     <div className="modalOverlay" onClick={() => setShowEdit(false)}></div>
     <div className='modal'>
-      <div>
-        Shortcut: {shortcut}
+      <div className="modalHeader">
+        <span>Edit Pelumi Link</span>
       </div>
-      <div>
-        Long Url: <input value={longUrl} onChange={(e)=> setLongUrl(e.target.value)}></input>
+      <div className="modalContent">
+        <div>
+          Shortcut: {shortcut}
+        </div>
+        <div>
+          Long Url: <input value={longUrl} onChange={(e)=> setLongUrl(e.target.value)}></input>
+        </div>
       </div>
-      <button onClick={() => {updateURl(); setShowEdit(false)}}>Update</button>
-      <button onClick={() => setShowEdit(false)}>close</button>
+      <div className="modalActions">
+        <button style={{marginRight:'10px'}} className='actionBtn leftBtn'  onClick={() => setShowEdit(false)}>Cancel</button>
+        <button className='actionBtn rightBtn' onClick={() => {updateURl(); setShowEdit(false)}}>Save</button>
+      </div> 
     </div>
     </>
   ) 
